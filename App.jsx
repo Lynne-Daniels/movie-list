71dd51ddcfe3a28812673f46fe6a66c858681a51
Lynne Daniels <lynne33477@gmail.com>
2017-09-15 23:00:15 -0700

    fixed app.js, removed blog post files

--- App.jsx
+++ App.jsx
@@ -10,7 +10,20 @@ class App extends React.Component {
 
   }
   render() {
-    return <h1>{this.state.movie}</h1>;
+    return (
+      <div> 
+        <ul>
+          {this.props.movies.map((movie, index)=>{
+            return <li key={index}>{movie.title}:
+              <p><code>popcorn</code></p></li>;
+          }
+          )}
+        </ul>
+        <h2>Hello Worldish Examples</h2>
+        <p><code>this.state.step </code>renders the same thing as <code>this.props.todos[0].step</code></p>
+        <p>{this.props.movies[0].title}</p>
+        <p>{this.state.movie.title}</p>
+      </div>);
   }
 }
 // App needs to be in the global scope.  It is undefined if this line is removed

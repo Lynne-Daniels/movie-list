ec8774722a5dd493ed2371557d87a5b3a28280b6
Lynne Daniels <lynne33477@gmail.com>
2017-09-16 22:13:40 -0700

    search live-renders a filtered list from local data, added a little css

--- App.jsx
+++ App.jsx
@@ -3,26 +3,48 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      movies: this.props.movies,
-      movie: this.props.movies[0].title
-    }; 
+    this.state = {value: ''};
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('A name was submitted: ' + this.state.value);
+    event.preventDefault();
   }
   render() {
     return (
       <div> 
+        {/*https://facebook.github.io/react/docs/forms.html*/}
+        <input onChange={this.handleChange} type="text">Search</input>
+        <div>{this.state.value}</div>
+        <h4>Filtered Movies</h4>
+        <ul>
+          {
+            //filter the movies
+            this.props.movies.filter((movie) =>{
+
+              return movie.title.indexOf(this.state.value) > -1;
+            // map results to component html
+            }).map((movie, index)=>{
+              return <li key={index}>{movie.title}</li>;
+            })
+          }
+        </ul>
+        <h4>All the movies</h4>
         <ul>
           {this.props.movies.map((movie, index)=>{
-            return <li key={index}>{movie.title}:
-              <p><code>popcorn</code></p></li>;
+            return <li key={index}>{movie.title}</li>;
           }
           )}
         </ul>
         <h2>Hello Worldish Examples</h2>
         <p><code>this.state.step </code>renders the same thing as <code>this.props.todos[0].step</code></p>
         <p>{this.props.movies[0].title}</p>
-        <p>{this.state.movie.title}</p>
       </div>);
   }
 }
--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 /// render to the DOM
 ReactDOM.render(<App movies = {window.movies}/>, document.getElementById('app'));
-
-
-
